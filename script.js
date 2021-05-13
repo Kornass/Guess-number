00754169af592a40905e4fd297ca0cbe3a829523
Pawel Kornas <pawelkornases@gmail.com>
2021-05-13 16:39:00 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,48 @@
+'use strict';
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+const points = function (score1) {
+  document.querySelector('.number').textContent = score1;
+};
+const lives = function (live1) {
+  document.querySelector('.score').textContent = live1;
+};
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  if (!guess) {
+    displayMessage('📣No number!');
+  } else if (guess === secretNumber) {
+    displayMessage('🎉 You win!');
+    points(secretNumber);
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? '⬆ Too high !' : '⬇ Too low');
+      score--;
+      lives(score);
+    } else {
+      displayMessage('😥 You lost the game !');
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
+document.querySelector('.again').addEventListener('click', function () {
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  score = 20;
+  displayMessage('Start guessing...');
+  lives(score);
+  points('?');
+  document.querySelector('.guess').value = '';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+});
